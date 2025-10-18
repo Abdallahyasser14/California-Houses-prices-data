37fe17f47d53c539a750762a088b95a121528521
abdallah yasser <abdallahshady4@gmail.com>
2025-10-18 20:40:56 +0300

    gradient descent models

--- src/models/linear_regression/closed_form.py
+++ src/models/linear_regression/closed_form.py
@@ -12,10 +12,14 @@ class linear_regression_closed_form:
 
         self.parameters= np.linalg.inv(x_matrix_transpose @ x_matrix + self.lambda_param * np.identity(x_matrix.shape[1])) @ (x_matrix_transpose @ y_matrix )
         # [b1,w1,w2,...,wn]^T
-
+        
     def predict(self,X):
         X=X.copy()
         X["bias"] = 1
         return X.to_numpy() @ self.parameters
+    
+    
+    
+    
 
 
--- src/models/linear_regression/gradient_descent.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-
-class linear_regression_gradient_descent:
